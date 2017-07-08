23e36a401fc0af90f0c243ee7476ea810370d879
ihsavru <vurvashi11@gmail.com>
2017-07-08 10:17:47 +0530

    Fix js
    Snake does not move before the game starts.

--- snake.js
+++ snake.js
@@ -5,10 +5,11 @@ var snake = [
 	{x: 0,
 	y: 0}
 ]
+var start = 0;
 var foodx = 50,foody =50;
 var key ='';
 function startGame(){
-    var canvas = document.getElementById('canvas'); 
+    var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
     ctx.clearRect(foodx,foody,20,20);
     clearInterval(draw);
@@ -24,18 +25,18 @@ function startGame(){
 }
 
 function drawSnake(X,Y){
-    var canvas = document.getElementById('canvas'); 
+    var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
-    ctx.fillStyle = '#cccccc'; 
+    ctx.fillStyle = '#cccccc';
 	for (var i = 0; i < length; i++) {
 		ctx.fillRect(snake[i].x,snake[i].y,20,20);
 
 	}
-	
+
 }
 
 function drawFood(){
-    var canvas = document.getElementById('canvas'); 
+    var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
     ctx.fillStyle = '#48cec1';
     foodx = Math.floor(Math.random()*24)*20;
@@ -50,7 +51,7 @@ function drawFood(){
 }
 
 function moveSnake(X,Y) {
-	var canvas = document.getElementById('canvas'); 
+	var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
 	for (var i = 0; i < length; i++) {
 		ctx.clearRect(snake[i].x,snake[i].y,20,20);
@@ -64,7 +65,7 @@ function moveSnake(X,Y) {
 }
 
 function checkCollision(){
-         
+
         //checks if the snake collided with the food i.e score increases
         if(snake[0].x === foodx && snake[0].y === foody){
             if(length >= 2){
@@ -77,14 +78,14 @@ function checkCollision(){
             printScore();
             drawFood();
         }
-        
+
         //checks if the snake collided with the canvas border i.e game over
         if(snake[0].x === 500 || snake[0].y === 500 || snake[0].x === -20 || snake[0].y === -20){
             alert("game over");
             clearInterval(draw);
             startGame();
         }
-        
+
         //check if the snake collided with itself i.e. game over
         for(var i=3;i<length;i++){
             if(snake[0].x === snake[i].x && snake[0].y === snake[i].y){
@@ -104,44 +105,48 @@ $(function(){
 	$('#start').on('click', function(){
 		startGame();
 		$(this).hide();
-		var canvas = document.getElementById('canvas'); 
+		var canvas = document.getElementById('canvas');
         var ctx = canvas.getContext('2d');
         ctx.fillStyle = '#cccccc';
         ctx.fillRect(snake[0].x,snake[0].y,20,20);
+        start++;
 	})
 
 	document.onkeydown = function(e) {
+			  if(start>0){
     switch (e.keyCode) {
-        case 37:
-            if(key !== 'r'){
-                clearInterval(draw);
-                draw = setInterval(function(){moveSnake(-1,0);},100);
-                key = 'l';
-                
-            }
-            break;
-        case 38:
-            if(key!== 'd'){
-                clearInterval(draw);
-                draw = setInterval(function(){moveSnake(0,-1);}, 100);
-                key = 'u';
-            }
-            break;
-        case 39:
-            if(key!== 'l'){
-                clearInterval(draw);
-                draw = setInterval(function(){moveSnake(1,0);},100);
-                key = 'r';
-            }
-            break;
-        case 40:
-            if(key!== 'u'){
-                clearInterval(draw);
-                draw = setInterval(function(){moveSnake(0,1);},100);
-                key = 'd';
-            }
-            break;
-    }
+
+	        case 37:
+	            if(key !== 'r'){
+	                clearInterval(draw);
+	                draw = setInterval(function(){moveSnake(-1,0);},100);
+	                key = 'l';
+
+	            }
+	            break;
+	        case 38:
+	            if(key!== 'd'){
+	                clearInterval(draw);
+	                draw = setInterval(function(){moveSnake(0,-1);}, 100);
+	                key = 'u';
+	            }
+	            break;
+	        case 39:
+	            if(key!== 'l'){
+	                clearInterval(draw);
+	                draw = setInterval(function(){moveSnake(1,0);},100);
+	                key = 'r';
+	            }
+	            break;
+	        case 40:
+	            if(key!== 'u'){
+	                clearInterval(draw);
+	                draw = setInterval(function(){moveSnake(0,1);},100);
+	                key = 'd';
+	            }
+	            break;
+	    }
+		}
 };
-    
+
 });
